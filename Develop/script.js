b1a7e2502f914ed4324aba02d3946058b68477c0
jjsmith32199 <113246711+jjsmith32199@users.noreply.github.com>
2022-12-11 20:43:51 -0500

    Functionality of generator added. Readme file completed

--- Develop/script.js
+++ Develop/script.js
@@ -1,108 +1,124 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var specialChar = ["!@#$%^&*()-=_+"];
-var lowerCase = ['abcdefghijklmnopqrstuvwxyz'];
-var upperCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
-var numbers = ['1234567890'];
-var passwordLen;
-var upperCase;
-var lowerCase;
+let generateBtn = document.querySelector("#generate");
+let specialChar = ["!@#$%^&*()-=_+"];
+let lowerCase = ['abcdefghijklmnopqrstuvwxyz'];
+let upperCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+let numbers = ['1234567890'];
 
- function checkpassLen() {
-   passwordLen = prompt('How long should we make password?(8-128 characters');
-  if (passwordLen > 128){
-    alert('Too many characters selected. Please select a number between 8 and 128');
-    checkpassLen();
-  } 
-  if(passwordLen < 8){
-    alert('Too few characters selected. Please select a number betweeen 8 and 128');
-    checkpassLen();
+ // generate button function was decalred and never defined
+ // the variables that make up the password generator also needed to be defined
+ // the following functions will present the end user with choice criteria for their password
+function checkpassLen() {
+ let passwordLen = parseInt(prompt('How long should we make password? (8-128 characters'));
+ confirm('You have selected ' + passwordLen + ' characters.');
+ if (passwordLen > 128){
+ alert('Too many characters selected. Please select a number between 8 and 128.');
+ checkpassLen();
+ }
+ else if(passwordLen < 8){
+ alert('Too few characters selected. Please select a number betweeen 8 and 128.');
+ checkpassLen();
+   }
+ else if (isNaN(passwordLen)){
+ alert('Please enter a numerical value between 8 and 128.')
+ checkpassLen();
   }
-  else if (isNaN(passwordLen)){
-    alert('Please enter a numerical value between 8 and 128')
-    checkpassLen();
-  }
-}
+  else{
  
-
- function specChar(){
-  specialChar = prompt('Would you like to include special characters? Type y or n');
-    if (specialChar === 'y') {
-      console.log('Special characters selected');
-    }
-    else if (specialChar === 'n') { 
-      console.log('No special characters included');
-    }
-    else {
-    alert('Please enter y or n to choose');
-    specChar()
-    }
   }
+  return passwordLen
+}
+function CheckspecChar(){
+ let specialChar = prompt('Would you like to include special characters? Type y or n.');
+ confirm("You have selected " + specialChar + " for special characters.");
+ if (specialChar === 'y') {
+ console.log('Special characters selected.');
+     }
+ else if (specialChar === 'n') { 
+ console.log('No special characters included.');
+     }
+ else {
+ alert('Please enter y or n to choose.');
+ specChar()
+ }
+ return specialChar
+}
 function checknumber(){
-  numbers = prompt('Would you like to include numbers? Type y or n');
-    if(numbers === 'y'){
-      console.log('Numbers selected');
-    }
-  else if (numbers === 'n'){
-  console.log('No numbers selected');
-} 
-  else {
-    alert('Please enter y or n to choose');
-    checknumber()
+ let numbers = prompt('Would you like to include numbers? Type y or n.');
+ confirm("You have selected " + numbers + " for numbers.");
+ if(numbers === 'y'){
+ console.log('Numbers selected.');
   }
-}
- function checkCaselow(){
-  lowerCase = prompt('Do you want to include lower case letters? Type y or n');
-    if(lowerCase === 'y'){
-      console.log('Lower case letters selected');
-    }
-    else if ( lowerCase === 'n'){
-    console.log('Lower case letters not selected');
+ else if (numbers === 'n'){
+ console.log('No numbers selected.');
   }
-  else {
-    alert('Please enter y or n to choose');
-    checkCaselow()
+ else {
+ alert('Please enter y or n to choose.');
+ checknumber()
+ 
   }
+ return numbers
 }
-function checkCaseupper(){
-  upperCase = prompt('Do you want to include upper case letters? Type y or n');
-    if(lowerCase === 'y'){
-      console.log('Upper case letters selected');
-    }
-    else if ( upperCase === 'n'){
-    console.log('Upper case letters not selected');
+function checkCaselow(){
+ let lowerCase = prompt('Do you want to include lower case letters? Type y or n.');
+ confirm("You have selected " + lowerCase + " lower case letters.");
+ if(lowerCase === 'y'){
+ console.log('Lower case letters selected.');
   }
-  else {
-    alert('Please enter y or n to choose');
-    checkCaseupper()
+ else if ( lowerCase === 'n'){
+ console.log('Lower case letters not selected.');
   }
+ else {
+ alert('Please enter y or n to choose.');
+ checkCaselow()
+ 
+  }
+ return lowerCase
 }
- function generatePassword() {
-  checkpassLen();
-  specChar();
-  checknumber();
-  checkCaselow();
-  checkCaseupper();
+ function checkCaseupper(){
+ let upperCase = prompt('Do you want to include upper case letters? Type y or n');
+ confirm("You have selected " + upperCase + " lower case letters.");
+ if(upperCase === 'y'){
+ console.log('Upper case letters selected.');
+  }
+ else if ( upperCase === 'n'){
+ console.log('Upper case letters not selected.');
+  }
+ else {
+ alert('Please enter y or n to choose.');
+ checkCaseupper()
+  }
+ return upperCase
 }
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+ //Write password to the #password input
+ function writePassword(){
+ var password = generatePassword();
+ var passwordText = document.querySelector("#password");
+ 
+ passwordText.value = password;
+ 
 }
-
-
-
-
-
-
-
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// We need to store the choices selected and add them to the index for generation
+ function generatePassword(){
+ let choiceIndex= [checkpassLen(), CheckspecChar(), checkCaseupper(), checkCaselow(), checknumber()];
+ let characters = "";
+ let password = "";
+  if (choiceIndex[1]){
+    characters = characters + specialChar;
+  }
+  if (choiceIndex[2]){
+    characters = characters + upperCase;
+  }
+  if (choiceIndex[3]){
+    characters = characters + lowerCase;
+  }
+  if (choiceIndex[4]){
+    characters = characters + numbers;
+  }
+ for(var i = 0; i < choiceIndex[0]; i++) {
+ let randomIndex = Math.floor(Math.random() * characters.length);  
+ password = password + characters[randomIndex];
+ }
+ return password;
+ }
+ // Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword);
---
+++ Readme.md
@@ -0,0 +1,9 @@
+The following application is a refactoring of code for a random password generator. 
+This application will generate the user a randomized password based on the inclusion or exclusion of:
+1. Special characters
+2. Upper case letters
+3. Lower case letters
+4. Numbers
+Each criteria is presented as a prompt to the user and is recorded via the console log. The console will also log when the user presses the 'Generate password' button
+The prompts will ask the user to input a specific response to the questions asked and will then confirm their selection and prompt for a correction if needed. 
+Once all prompts are completed, the user will be presented a randomized password based on their selections.
